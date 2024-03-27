212f5287056840321651e0752af699eaffaaf065
miguelrp99 <miguelrodriguez.p99@gmail.com>
2024-03-27 03:37:01 +0100

    background changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#03040B] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))]"></div>
       <body className={inter.className}>{children}</body>
     </html>
   );
